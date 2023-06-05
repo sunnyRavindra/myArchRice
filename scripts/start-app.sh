a04ba4c9aed11abb29e936e622d7ce3bb80ba30c
Sunny Ravindra <sunnyravbusiness@gmail.com>
2023-06-05 20:24:41 +0530

    first commit

---
+++ scripts/start-app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yes | pacman -S neovim 
+yes | pacman -S base-devel
+yes | pacman -S freetype2 
+yes | pacman -S libxinerama 
+yes | pacman -S libxft
+yes | pacman -S libx11 
+yes | pacman -S fontconfig 
+yes | pacman -S ttf-dejavu
+yes | pacman -S alacritty
+yes | pacman -S firefox
+yes | pacman -S github-cli
