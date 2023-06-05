891a8acbc64254975f1158a2bafdb6bfea73deb3
Sunny Ravindra <sunnyravbusiness@gmail.com>
2023-06-05 22:50:11 +0530

    second Commit

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -11,3 +11,12 @@ yes | pacman -S ttf-dejavu
 yes | pacman -S alacritty
 yes | pacman -S firefox
 yes | pacman -S github-cli
+yes | pacman -S code
+yes | pacman -S picom
+yes | pacman -S python-pywal
+yes | pacman -S nitrogen
+yes | pacman -S feh
+
+sudo -u sunny cp ~/.xinitrc ~/myArchRice/.xinitrc
+sudo -u sunny cp ~/.config/picom.conf ~/myArchRice/picom.conf
+sudo -u sunny cp ~/.config/alacritty/alacritty.yml ~/myArchRice/alacritty.yml
