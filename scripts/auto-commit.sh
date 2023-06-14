2e9dca826d5439dfe4f6d9636ba43c80be94c509
Enakonda <97044520+TuttuRavindra@users.noreply.github.com>
2023-06-14 18:24:26 +0530

    Auto commit: 2023-06-14 18:24:26

---
+++ scripts/auto-commit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Directory where your Git repositories are located
+repo_dir="$HOME/code"
+
+while true; do
+    # Change to the repository directory
+    cd "$repo_dir"
+
+    # Iterate through each repository
+    for repo in */; do
+        # Change to the repository directory
+        cd "$repo"
+
+        # Check if there are any unstaged changes
+        if [[ -n $(git status --porcelain) ]]; then
+            # Add all changes
+            git add -A
+
+            # Commit changes with a default commit message
+            git commit -m "Auto commit: $(date '+%Y-%m-%d %H:%M:%S')"
+
+            # Push changes to the current branch
+            git push origin HEAD
+        fi
+
+        # Change back to the original directory
+        cd "$repo_dir"
+    done
+
+    # Sleep for 3 minutes
+    sleep 180
+done
